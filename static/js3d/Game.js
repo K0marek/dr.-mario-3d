a435e31490711612c1267e1260a6fc8f580cd8ef
McWojownik <quinlanvos1234@gmail.com>
2019-05-04 20:22:24 +0200

    new Class

--- static/js3d/Game.js
+++ static/js3d/Game.js
@@ -22,9 +22,6 @@ class Game {
         this.axes = new THREE.AxesHelper(1000)
         this.scene.add(this.axes)
 
-        this.raycaster = new THREE.Raycaster()
-        this.mouseVector = new THREE.Vector2()
-
         this.camera.position.set(0, 100, 600)
         this.camera.lookAt(this.scene.position.x, this.scene.position.x + 150, this.scene.position.z)
 
@@ -43,7 +40,7 @@ class Game {
             new Pill(this.randomColor(), this.randomColor())
         ]
 
-        this.pillsContainer = new THREE.Object3D
+        this.pillsContainer = new PillsContainer()
         this.scene.add(this.pillsContainer)
 
         this.speed = null // domyślna szybkość spadania pigułki
---
+++ static/js3d/PillsContainer.js
@@ -0,0 +1,8 @@
+console.log("wczytano plik PillsContainer.js")
+
+class PillsContainer extends THREE.Object3D {
+    constructor() {
+        super()
+        return this
+    }
+}
