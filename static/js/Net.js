a82935ed037a595cb3e69b5850f7967d16947866
Mateusz Kleszcz <kleeeszcz@gmail.com>
2019-06-05 10:44:10 +0200

    added multiplayer

---
+++ static/js/Net.js
@@ -0,0 +1,70 @@
+class Net {
+    constructor() {
+        this.client = io()
+        this.id = null // id gracza
+        this.enemy = null // id przeciwnika
+        this.which = null // który z kolei user
+
+        this.client.on('both', data => {
+            const { which, nick, id, enemy } = data
+            this.id = id
+            this.enemy = enemy
+            this.which = which
+
+            $('#loading').remove()
+            $('#twoPlayersSettings').css('display', 'block')
+            $('#me').text(`twoje id to: ${id}`)
+            $('#enemy').text(`id przeciwnika: ${enemy}`)
+        })
+
+        this.client.on('changeLevel1p', data => {
+            $('#level1p').val(data.value)
+        })
+
+        this.client.on('changeLevel2p', data => {
+            $('#level2p').val(data.value)
+        })
+
+        this.client.on('changeSpeedButtons1p', data => {
+            $('.speedButtons1p')
+                .removeClass('btn-danger')
+                .addClass('btn-primary')
+            $($(`button[value=${data.value}]`)[0])
+                .removeClass('btn-primary')
+                .addClass('btn-danger')
+        })
+
+        this.client.on('changeSpeedButtons2p', data => {
+            $('.speedButtons2p')
+                .removeClass('btn-danger')
+                .addClass('btn-primary')
+            $($(`button[value=${data.value}]`)[1])
+                .removeClass('btn-primary')
+                .addClass('btn-danger')
+        })
+
+        this.client.on('startGame', data => {
+            $('#menu').remove()
+            game.enemyBottle = new Bottle(16, 10)
+            game.scene.add(game.enemyBottle)
+            if (this.which % 2 == 0) { // pierwszy user
+                game.bottle.position.x = -240
+                game.enemyBottle.position.x = 60
+                game.play(settings.defaultSpeed, -160)
+            }
+            if (this.which % 2 == 1) { // drugi user
+                game.bottle.position.x = 60
+                game.enemyBottle.position.x = -240
+                game.play(settings.defaultSpeed, 140)
+            }
+        })
+    }
+
+    addUser = nick => {
+        this.nick = nick
+        //emitiowanie połączenia
+        this.client.emit('login', {
+            nick
+        })
+    }
+}
--- static/js/Ui.js
+++ static/js/Ui.js
@@ -2,22 +2,22 @@ console.log("wczytano plik Ui.js")
 
 class Ui {
 
-    constructor() {}
+    constructor() { }
 
     //UTWORZENIE STEROWANIA W GRZE
     controls = () => {
         $(document).on('keydown', e => {
-            const {pill} = game
-            const {cellSize} = settings
-            const {fields} = game.bottle
-            switch(e.keyCode) {
+            const { pill } = game
+            const { cellSize } = settings
+            const { fields } = game.bottle
+            switch (e.keyCode) {
                 //Z
                 case 90:
                     pill.positionSet++
-                    if(pill.positionSet == 0) {
+                    if (pill.positionSet == 0) {
                         pill.rotation.z = Math.PI / 2
                         pill.position.y -= cellSize
-                        if(!fields[pill.children[1].posY - 1][pill.children[1].posX + 1].allow) {
+                        if (!fields[pill.children[1].posY - 1][pill.children[1].posX + 1].allow) {
                             pill.position.x -= cellSize
                             pill.children[0].posX--
                             pill.children[0].posY--
@@ -26,8 +26,8 @@ class Ui {
                             pill.children[0].posY--
                         }
                     }
-                    else if(pill.positionSet == 1) {
-                        if(fields[pill.children[0].posY + 1][pill.children[0].posX].allow) {
+                    else if (pill.positionSet == 1) {
+                        if (fields[pill.children[0].posY + 1][pill.children[0].posX].allow) {
                             pill.rotation.z = Math.PI
                             pill.children[1].posX--
                             pill.children[1].posY++
@@ -35,9 +35,9 @@ class Ui {
                             pill.positionSet = 0
                         }
                     }
-                    else if(pill.positionSet == 2) {
-                        if(!fields[pill.children[1].posY - 1][pill.children[1].posX + 1].allow) {
-                            if(fields[pill.children[1].posY - 1][pill.children[1].posX - 1].allow) {
+                    else if (pill.positionSet == 2) {
+                        if (!fields[pill.children[1].posY - 1][pill.children[1].posX + 1].allow) {
+                            if (fields[pill.children[1].posY - 1][pill.children[1].posX - 1].allow) {
                                 pill.rotation.z = Math.PI * 1.5
                                 pill.children[1].posX--
                                 pill.children[1].posY--
@@ -50,8 +50,8 @@ class Ui {
                             pill.children[1].posY--
                         }
                     }
-                    else if(pill.positionSet == 3) {
-                        if(fields[pill.children[1].posY + 1][pill.children[1].posX].allow) {
+                    else if (pill.positionSet == 3) {
+                        if (fields[pill.children[1].posY + 1][pill.children[1].posX].allow) {
                             pill.positionSet = -1
                             pill.position.x -= cellSize
                             pill.position.y += cellSize
@@ -65,7 +65,7 @@ class Ui {
                     break
                 //LEFT
                 case 37:
-                    if(game.checkPossibility('-')) {
+                    if (game.checkPossibility('-')) {
                         pill.position.x -= 20
                         pill.children.forEach(half => {
                             half.posX--
@@ -74,7 +74,7 @@ class Ui {
                     break
                 //RIGHT
                 case 39:
-                    if(game.checkPossibility('+')) {
+                    if (game.checkPossibility('+')) {
                         pill.position.x += 20
                         pill.children.forEach(half => {
                             half.posX++
@@ -89,7 +89,7 @@ class Ui {
         })
 
         $(document).on('keyup', e => {
-            switch(e.keyCode) {
+            switch (e.keyCode) {
                 case 40:
                     game.speed = settings.defaultSpeed
                     break
@@ -99,12 +99,92 @@ class Ui {
 
     //OBSŁUGA KLIKNIĘĆ
     interface = () => {
-        $('#playButton').on('click', () => {
+
+        $('#playButton').on('click', e => {
             game.play(settings.defaultSpeed)
+            $('#menu').remove()
             $("#controls").empty()
             let dv = $("<div>").text("Twój wynik: 0").prop("id", "score")
             $("#controls").append(dv)
         })
+
+        $('#onePlayer').on('click', e => {
+            $('#start').css('display', 'none')
+            $('#onePlayerSettings').css('display', 'block')
+        })
+
+        $('#twoPlayers').on('click', e => {
+            $('#start').css('display', 'none')
+            $('#login').css('display', 'block')
+        })
+
+        $('#search').on('click', e => {
+            const nick = $('#nick').val()
+            const loading = $('<div>').attr('id', 'loading')
+            const spinner = $('<div>').addClass('spinner-border')
+            const header = $('<p>').html('Trwa oczekiwanie na innego gracza')
+            loading
+                .append(header)
+                .append(spinner)
+            $('#login').empty()
+            $('#menu').append(loading)
+            net.addUser(nick)
+        })
+
+        // obsługa inputów i ich odwzorowanie na drugim graczu
+        $('#level1p').on('input', e => {
+            const value = e.target.value
+            net.client.emit('changeLevel1p', {
+                value,
+                enemy: net.enemy
+            })
+        })
+
+        $('#level2p').on('input', e => {
+            const value = e.target.value
+            net.client.emit('changeLevel2p', {
+                value,
+                enemy: net.enemy
+            })
+        })
+
+        $('.speedButtons1p').on('click', e => {
+            $('.speedButtons1p')
+                .removeClass('btn-danger')
+                .addClass('btn-primary')
+            $(e.target)
+                .removeClass('btn-primary')
+                .addClass('btn-danger')
+
+            const value = e.target.value
+            net.client.emit('changeSpeedButtons1p', {
+                value,
+                enemy: net.enemy
+            })
+        })
+
+        $('.speedButtons2p').on('click', e => {
+            $('.speedButtons2p')
+                .removeClass('btn-danger')
+                .addClass('btn-primary')
+            $(e.target)
+                .removeClass('btn-primary')
+                .addClass('btn-danger')
+
+            const value = e.target.value
+            net.client.emit('changeSpeedButtons2p', {
+                value,
+                enemy: net.enemy
+            })
+        })
+
+        $('#playButton2p').on('click', e => {
+            net.client.emit('startGame', {
+                id: net.id,
+                enemy: net.enemy
+            })
+        })
+
     }
 
     //USUNIĘCIE STEROWANIA W GRZE
